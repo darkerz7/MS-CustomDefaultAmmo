107b8869b1c6e08a4ffb3d1f3c7d440f1f85fc11
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2025-11-29 14:44:25 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# MS-CustomDefaultAmmo
+# MS-CustomDefaultAmmo for ModSharp
 Change the default ammo on a weapon
+
+## Required packages:
+1. [ModSharp](https://github.com/Kxnrl/modsharp-public)
+
+## Installation:
+1. Compile or copy MS-CustomDefaultAmmo to `sharp/modules/MS-CustomDefaultAmmo` folger
+2. Configure config file `config.json`
---
+++ src/CustomDefaultAmmo.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Extensions.Configuration;
+using Sharp.Shared;
+using Sharp.Shared.GameEntities;
+using Sharp.Shared.Listeners;
+using Sharp.Shared.Managers;
+using System.Text.Json;
+
+namespace MS_CustomDefaultAmmo
+{
+    public class CustomDefaultAmmo : IModSharpModule, IGameListener, IEntityListener
+    {
+        public string DisplayName => "CustomDefaultAmmo";
+        public string DisplayAuthor => "DarkerZ[RUS]";
+        public CustomDefaultAmmo(ISharedSystem sharedSystem, string dllPath, string sharpPath, Version version, IConfiguration coreConfiguration, bool hotReload)
+        {
+            _modSharp = sharedSystem.GetModSharp();
+            _entities = sharedSystem.GetEntityManager();
+            _dllPath = dllPath;
+        }
+
+        private readonly IModSharp _modSharp;
+        private readonly IEntityManager _entities;
+        private readonly string _dllPath;
+
+        ConfigWeapons? cfg = new();
+
+        public bool Init()
+        {
+            _modSharp.InstallGameListener(this);
+            _entities.InstallEntityListener(this);
+            return true;
+        }
+
+        public void Shutdown()
+        {
+            _modSharp.RemoveGameListener(this);
+            _entities.RemoveEntityListener(this);
+        }
+
+        public void OnEntitySpawned(IBaseEntity entity)
+        {
+            if (cfg != null && entity.IsValid() && entity.AsBaseWeapon() is { } weapon)
+            {
+                foreach (var item in cfg.Weapons.ToArray())
+                {
+                    if (item.DefIndex != 0 && item.DefIndex == weapon.AttributeContainer.Item.ItemDefinitionIndex)
+                    {
+                        if (item.Clip <= -1)
+                        {
+                            weapon.GetWeaponData().MaxClip = item.Clip;
+                            weapon.Clip = item.Clip;
+                        }
+                        if (item.ReserveAmmo <= -1)
+                        {
+                            weapon.GetWeaponData().PrimaryReserveAmmoMax = item.ReserveAmmo;
+                            weapon.ReserveAmmo = item.ReserveAmmo;
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void OnGameActivate() //OnMapStart
+        {
+            LoadCFG();
+        }
+
+        public void OnGameDeactivate() //OnMapEnd
+        {
+            cfg = null;
+        }
+
+        void LoadCFG()
+        {
+            string sConfig = $"{Path.Join(_dllPath, "config.json")}";
+            string sData;
+            if (File.Exists(sConfig))
+            {
+                try
+                {
+                    sData = File.ReadAllText(sConfig);
+                    cfg = JsonSerializer.Deserialize<ConfigWeapons>(sData);
+                    if (cfg != null) //Validate
+                    {
+                        foreach (var weapon in cfg.Weapons.ToArray())
+                        {
+                            weapon.SetDefIndex();
+                        }
+                    }
+                }
+                catch
+                {
+                    cfg = null;
+                    PrintToConsole($"Bad Config file ({sConfig})");
+                }
+            }
+            else
+            {
+                cfg = null;
+                PrintToConsole($"Config file ({sConfig}) not found");
+            }
+        }
+
+        public static void PrintToConsole(string sValue)
+        {
+            Console.ForegroundColor = (ConsoleColor)8;
+            Console.Write("[");
+            Console.ForegroundColor = (ConsoleColor)6;
+            Console.Write("CustomDefaultAmmo");
+            Console.ForegroundColor = (ConsoleColor)8;
+            Console.Write("] ");
+            Console.ForegroundColor = (ConsoleColor)3;
+            Console.WriteLine(sValue);
+            Console.ResetColor();
+        }
+
+        int IGameListener.ListenerVersion => IGameListener.ApiVersion;
+        int IGameListener.ListenerPriority => 0;
+        int IEntityListener.ListenerVersion => IEntityListener.ApiVersion;
+        int IEntityListener.ListenerPriority => 0;
+    }
+}
